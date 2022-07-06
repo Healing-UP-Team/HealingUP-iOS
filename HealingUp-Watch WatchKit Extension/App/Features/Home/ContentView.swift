1077a513a05d23fafd2c47e9f36b3bd29e4135ca
nbsmobile <buwonodirga@gmail.com>
2022-07-06 23:51:40 +0700

    build(Build): Lock potrait and change display name

--- HealingUp-Watch WatchKit Extension/App/Features/Home/ContentView.swift
+++ HealingUp-Watch WatchKit Extension/App/Features/Home/ContentView.swift
@@ -10,7 +10,7 @@ import HealthKit
 
 public enum MenuWatch {
   case heart
-  case history
+  case profile
 }
 
 struct HomeMenu: Hashable {
@@ -23,9 +23,10 @@ struct ContentView: View {
   @ObservedObject var heart = HeartRateViewModel()
 
   private var menu: [HomeMenu] = [
-    .init(imageName: "heart.fill", title: "Heart", menuType: .heart),
-    .init(imageName: "clock.arrow.circlepath", title: "History", menuType: .history)
+    .init(imageName: "bolt.heart", title: "Heart", menuType: .heart),
+    .init(imageName: "person.fill", title: "Profile", menuType: .profile)
   ]
+  
   var body: some View {
     ScrollView {
       ForEach(menu, id: \.self) { item in
@@ -45,6 +46,7 @@ struct ContentView: View {
     HStack(spacing: 0) {
       Image(systemName: menu.imageName)
         .font(.system(size: 25))
+        .foregroundColor(Color.white)
       
       Text(menu.title)
         .foregroundColor(Color.white)
@@ -54,6 +56,7 @@ struct ContentView: View {
       Spacer()
       Image(systemName: "chevron.forward")
         .font(.system(size: 10, weight: .medium))
+        .foregroundColor(Color.white)
       
     }
   }
@@ -62,8 +65,8 @@ struct ContentView: View {
     switch menu {
     case .heart:
       return AnyView(HeartRateVariabilityView())
-    case .history:
-      return AnyView(Text("History"))
+    case .profile:
+      return AnyView(Text("Profile"))
     }
   }
 }
@@ -71,33 +74,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
-  }
-}
-
-
-extension Color {
-  init(hex: String) {
-    let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-    var int: UInt64 = 0
-    Scanner(string: hex).scanHexInt64(&int)
-    let a, r, g, b: UInt64
-    switch hex.count {
-    case 3: // RGB (12-bit)
-      (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-    case 6: // RGB (24-bit)
-      (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-    case 8: // ARGB (32-bit)
-      (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-    default:
-      (a, r, g, b) = (1, 1, 1, 0)
-    }
-    
-    self.init(
-      .sRGB,
-      red: Double(r) / 255,
-      green: Double(g) / 255,
-      blue:  Double(b) / 255,
-      opacity: Double(a) / 255
-    )
   }
 }
