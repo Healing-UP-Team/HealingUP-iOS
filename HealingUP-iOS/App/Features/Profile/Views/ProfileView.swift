a19604c731c8ef8f9ab0c8f98e1ba3305927ab48
Dicky-27 <buwonodirga@gmail.com>
2022-07-14 21:06:54 +0700

    feat(ProfileView): Adjust UI

--- HealingUP-iOS/App/Features/Profile/Views/ProfileView.swift
+++ HealingUP-iOS/App/Features/Profile/Views/ProfileView.swift
@@ -7,30 +7,124 @@
 
 import SwiftUI
 
-struct ProfileView: View {
+struct ProfileMenu: Hashable {
+  var menu: ProfileMenuEnum
+  var image: String
+}
 
+enum ProfileMenuEnum: String {
+  case personalInfo = "Personal Information"
+  case aboutUs = "About Us"
+  case terms = "General Terms of Use"
+}
+
+struct ProfileView: View {
   @ObservedObject var viewModel: MembershipViewModel
   let navigator: ProfileNavigator
-  @State var name = ""
   @State var isSignOut = false
   @State var profile: User?
+  @State private var isShowAlert = false
+  let profileMenuSectionOne: [ProfileMenu] = [
+    .init(menu: .personalInfo, image: "person.crop.circle")
+  ]
+  
+  let profileMenuSectionTwo: [ProfileMenu] = [
+    .init(menu: .aboutUs, image: "person.2"),
+    .init(menu: .terms, image: "doc.text")
+  ]
 
   var body: some View {
-    VStack {
+    ScrollView(showsIndicators: false) {
       VStack {
-        Text("Hi!")
-        Text(name)
+        Image(uiImage: .appIcon)
+          .resizable()
+          .scaledToFit()
+          .frame(height: UIScreen.main.bounds.height/4)
+          .padding()
+        
+        Text(profile?.name ?? "Your Name")
+          .font(.system(size: 20, weight: .semibold))
+          .padding(.bottom)
 
+        
+        ForEach(profileMenuSectionOne, id: \.self) { item in
+          NavigationLink(
+            destination:
+              Group {
+                if item.menu == .personalInfo {
+                  Text(item.menu.rawValue)
+                }
+                if item.menu == .aboutUs {
+                  Text(item.menu.rawValue)
+                }
+                if item.menu == .terms {
+                  Text(item.menu.rawValue)
+                }
+              }
+          ) {
+            HStack() {
+              Image(systemName: item.image)
+                .foregroundColor(Color.gray)
+                .font(.system(size: 20))
+                .frame(width: 30)
+              Text(item.menu.rawValue)
+                .foregroundColor(Color.black)
+              Spacer()
+              Image(systemName: "chevron.right")
+                .foregroundColor(Color.gray)
+            }
+            .padding(.horizontal)
+            .padding(.top)
+          }
+        }
+        
+        Divider()
+          .padding()
+        
+        ForEach(profileMenuSectionTwo, id: \.self) { item in
+          NavigationLink(
+            destination:
+              Group {
+                if item.menu == .aboutUs {
+                  Text(item.menu.rawValue)
+                }
+                if item.menu == .terms {
+                  Text(item.menu.rawValue)
+                }
+              }
+          ) {
+            HStack() {
+              Image(systemName: item.image)
+                .foregroundColor(Color.gray)
+                .font(.system(size: 20))
+                .frame(width: 30)
+              Text(item.menu.rawValue)
+                .foregroundColor(Color.black)
+              Spacer()
+              Image(systemName: "chevron.right")
+                .foregroundColor(Color.gray)
+            }
+            .padding(.horizontal)
+            .padding(.top)
+          }
+        }
         Button {
-          viewModel.signOutUser()
+          isShowAlert = true
         } label: {
-          Text("Logout")
+          HStack {
+            Image(systemName: "escape")
+              .foregroundColor(Color.gray)
+              .font(.system(size: 20))
+              .frame(width: 30)
+            Text("Logout")
+              .foregroundColor(Color.black)
+            Spacer()
+          }.padding()
         }
       }
       .onViewStatable(
         viewModel.$userState,
         onSuccess: { data in
-          name = data.email
           profile = data
 
         })
@@ -45,6 +139,16 @@ struct ProfileView: View {
     }
     .fullScreenCover(isPresented: $isSignOut) {
       navigator.navigateToSignIn()
+    }
+    .alert(isPresented: $isShowAlert) {
+      Alert(
+        title: Text("Are you sure?"),
+        message: Text("You will be logged out and have to log in again."),
+        primaryButton: .destructive(Text("OK")) {
+          viewModel.signOutUser()
+        },
+        secondaryButton: .cancel()
+      )
     }
     .progressHUD(isShowing: $viewModel.userState.isLoading)
     .progressHUD(isShowing: $viewModel.signOutState.isLoading)
