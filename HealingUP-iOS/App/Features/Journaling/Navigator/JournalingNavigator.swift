17ef756da593dcb76d2746b911f04ec01f913ed2
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-07-25 22:29:43 +0700

    feat: make navigation for journal

---
+++ HealingUP-iOS/App/Features/Journaling/Navigator/JournalingNavigator.swift
@@ -0,0 +1,21 @@
+//
+//  JournalingNavigator.swift
+//  HealingUP-iOS
+//
+//  Created by Muhammad Rifki Widadi on 12/07/22.
+//
+
+import SwiftUI
+
+struct JournalingNavigator {
+    private let assembler: Assembler
+
+    init(assembler: Assembler) {
+      self.assembler = assembler
+    }
+
+    func navigateToJournalingTabItem() -> JournalingTabItem {
+        return JournalingTabItem(navigator: assembler.resolve(), journalingViewModel: assembler.resolve())
+    }
+}
+
