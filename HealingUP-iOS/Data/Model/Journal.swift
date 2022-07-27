40e71e49779d5824242b28da3eaf8c9034877de2
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-07-27 19:52:24 +0700

    feat: add jurnal model

---
+++ HealingUP-iOS/Data/Model/Journal.swift
@@ -0,0 +1,27 @@
+//
+//  JournalModel.swift
+//  Semhas
+//
+//  Created by Muhammad Rifki Widadi on 30/06/22.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+struct Journal: Identifiable, Codable {
+    @DocumentID var id: String?
+    var userId: String?
+    var emoji: String
+    var title: String
+    var note: String
+    var date: String
+
+  enum CodingKeys: String, CodingKey {
+    case id
+    case userId
+    case emoji
+    case title
+    case note
+    case date
+  }
+}
