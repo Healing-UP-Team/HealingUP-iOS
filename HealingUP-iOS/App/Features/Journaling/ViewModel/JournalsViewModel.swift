f424be868444b25a9b52436d2bc307c3f44e3e41
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-07-27 19:51:30 +0700

    feat: add view mdoel for listview

---
+++ HealingUP-iOS/App/Features/Journaling/ViewModel/JournalsViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  JournalViewModel.swift
+//  Semhas
+//
+//  Created by Muhammad Rifki Widadi on 13/07/22.
+//
+
+import Combine
+import FirebaseFirestore
+import Firebase
+
+class JournalsViewModel: ObservableObject {
+  @Published var journals = [Journal]()
+
+  private var db = Firestore.firestore()
+  private var listenerRegistration: ListenerRegistration?
+
+  private let path = "journals"
+
+  deinit {
+    unsubscribe()
+  }
+
+  func subscribe() {
+    guard let userId = Auth.auth().currentUser?.uid else { return }
+
+    if listenerRegistration == nil {
+      listenerRegistration = db.collection(path)
+        .whereField("userId", isEqualTo: userId)
+        .addSnapshotListener { (querySnapshot, error) in
+        guard let documents = querySnapshot?.documents else {
+          print("No documents")
+          return
+        }
+
+        self.journals = documents.compactMap { queryDocumentSnapshot in
+          try? queryDocumentSnapshot.data(as: Journal.self)
+        }
+      }
+    }
+
+  }
+
+  func unsubscribe() {
+    if listenerRegistration != nil {
+      listenerRegistration?.remove()
+      listenerRegistration = nil
+    }
+  }
+
+
+
+}
+
