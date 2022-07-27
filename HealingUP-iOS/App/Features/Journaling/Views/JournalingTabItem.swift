2d06330189ef7cfb40e1166623cc6ae502fb978d
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-07-27 19:52:12 +0700

    feat: add journal view for tab item

--- HealingUP-iOS/App/Features/Journaling/Views/JournalingTabItem.swift
+++ HealingUP-iOS/App/Features/Journaling/Views/JournalingTabItem.swift
@@ -1,80 +1,62 @@
 //
-//  JournalingTabItem.swift
-//  HealingUP-iOS
+//  JournalList.swift
+//  Semhas
 //
-//  Created by Muhammad Rifki Widadi on 12/07/22.
+//  Created by Muhammad Rifki Widadi on 30/06/22.
 //
 
 import SwiftUI
 
 struct JournalingTabItem: View {
-    let navigator: JournalingNavigator
-    @ObservedObject var journalingViewModel: JournalingViewModel
-
-    @ObservedObject var journalVM = JournalViewModel()
-    @State private var searchText = ""
-    @State private var showForm = false
+  @ObservedObject var viewModel = JournalsViewModel()
+  @State private var searchText = ""
+  @State private var showForm = false
 
 
-    var body: some View {
-            List {
-                ForEach(searchResult) { journal in
-                    NavigationLink(destination: JournalView(journal: journal)) {
-                        JournalCell(journal: journal)
-
-                    }.swipeActions(edge: .trailing, allowsFullSwipe: true) {
-                        Button {
-
-                        } label: {
-                            Label("Delete", systemImage: "trash")
-                        }
-                        .tint(.red)
-
-                        Button {
-
-                        } label: {
-                            Label("Edit", systemImage: "pencil")
-                        }
-                        .tint(.green)
-
-                }
-                }
-            }
-            .sheet(isPresented: $showForm) {
-                JournalForm()
-            }
-            .searchable(text: $searchText)
-            .listRowSeparator(.hidden)
-            .listStyle(.plain)
-            .navigationBarTitle("Journaling")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        showForm.toggle()
-                    } label: {
-                        Image(systemName: "square.and.pencil")
-                            .resizable()
-                            .frame(width: 24, height: 24)
-                            .foregroundColor(.accentPurple)
-                    }
-
-
-                }
-            }
-
-    }
-
-    var searchResult: [Journal] {
-        if searchText.isEmpty {
-            return journalVM.journal
-        } else {
-            return journalVM.journal.filter { $0.title.contains(searchText) }
+  var body: some View {
+    List {
+      ForEach(searchResult) { journal in
+        NavigationLink(destination: JournalDetailView(journal: journal)) {
+          JournalCell(journal: journal)
         }
+      }
+
     }
+    .listStyle(.inset)
+    .sheet(isPresented: $showForm) {
+      JournalEditView()
+    }
+    .searchable(text: $searchText)
+    .navigationBarTitle("Journaling")
+    .toolbar {
+      ToolbarItem(placement: .navigationBarTrailing) {
+        Button {
+          showForm.toggle()
+        } label: {
+          Image(systemName: "square.and.pencil")
+            .resizable()
+            .frame(width: 24, height: 24)
+            .foregroundColor(.accentPurple)
+        }
+      }
+    }
+    .onAppear() { // (1)
+      self.viewModel.subscribe()
+    }
+  }
+
+  var searchResult: [Journal] {
+    if searchText.isEmpty {
+      return viewModel.journals
+    } else {
+      return viewModel.journals.filter { $0.title.contains(searchText) }
+    }
+  }
+
 }
 
-struct JournalingTabItem_Previews: PreviewProvider {
-    static var previews: some View {
-        JournalingTabItem(navigator: AppAssembler.shared.resolve(), journalingViewModel: AppAssembler.shared.resolve())
-    }
+struct JournalList_Previews: PreviewProvider {
+  static var previews: some View {
+    JournalingTabItem()
+  }
 }
