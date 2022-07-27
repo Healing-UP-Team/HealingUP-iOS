6c3fa2d256a80e19520bf225205c1654b47338be
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-07-27 19:52:56 +0700

    feat: add ext color

--- HealingUP-iOS/App/Utils/Extensions/Color.swift
+++ HealingUP-iOS/App/Utils/Extensions/Color.swift
@@ -8,14 +8,7 @@
 import SwiftUI
 
 extension Color {
-    /**static let primaryWhite = UIColor(red: 255, green: 255, blue: 255, alpha: 1)
-     static let accentPurple = UIColor(red: 97, green: 94, blue: 168, alpha: 1)
 
-     //MARK: Kessler History
-     static let softYellow = UIColor(red: 251, green: 229, blue: 161, alpha: 1)
-     static let softPinkSecond = UIColor(red: 251, green: 231, blue: 216, alpha: 1)
-     static let softBlue = UIColor(red: 232, green: 240, blue: 251, alpha: 1)
-     static let softPinkFirst = UIColor(red: 245, green: 191, blue: 194, alpha: 1)*/
 
     static var primaryWhite = Color(uiColor: UIColor.primaryWhite)
     static var accentPurple = Color(uiColor: UIColor.accentPurple)
