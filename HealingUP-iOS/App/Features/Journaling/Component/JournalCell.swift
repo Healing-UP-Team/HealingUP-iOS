66b145a07fdc1ef855764261a446cd72b823468e
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-07-27 19:49:51 +0700

    feat: add journal cell

--- HealingUP-iOS/App/Features/Journaling/Component/JournalCell.swift
+++ HealingUP-iOS/App/Features/Journaling/Component/JournalCell.swift
@@ -8,54 +8,56 @@
 import SwiftUI
 
 struct JournalCell: View {
-    var journal: Journal
+  var journal: Journal
 
-    private var color: Color {
-        Color.combColor[self.journal.emoji]!
-    }
+  private var color: Color {
+    Color.combColor[self.journal.emoji] ?? .accentPurple
+  }
 
-    private var textColor: Color {
-        color == .accentPurple ? .white : .black
-    }
+  private var textColor: Color {
+    color == .accentPurple ? .white : .black
+  }
 
-    var body: some View {
-        HStack {
-            Text(journal.emoji)
-                .font(.system(size: 30))
-                .frame(width: 55, height: 49)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .strokeBorder(.white, lineWidth: 1)
-                )
-                .padding()
-            VStack(alignment: .leading, spacing: 8) {
-                Text(journal.title)
-                    .font(.system(size: 16))
-                    .fontWeight(.medium)
-                    .foregroundColor(textColor)
-                HStack(spacing: 8) {
-                    Image(systemName: "calendar")
-                        .font(.system(size: 10))
-                        .foregroundColor(textColor)
-                    Text(journal.date)
-                        .font(.system(size: 10))
-                        .foregroundColor(textColor)
-                }
-            }
-        }
-        .frame(width: 358, height: 80, alignment: .leading)
-        .background(
-            Rectangle()
-                .fill(color)
-                .cornerRadius(16)
+  var body: some View {
+    HStack {
+      Text(journal.emoji)
+        .font(.system(size: 30))
+        .frame(width: 55, height: 49)
+        .overlay(
+          RoundedRectangle(cornerRadius: 16)
+            .strokeBorder(.white, lineWidth: 1)
         )
+        .padding()
+      VStack(alignment: .leading, spacing: 8) {
+        Text(journal.title)
+          .font(.system(size: 16))
+          .fontWeight(.medium)
+          .foregroundColor(textColor)
+        HStack(spacing: 8) {
+          Image(systemName: "calendar")
+            .font(.system(size: 10))
+            .foregroundColor(textColor)
+          Text(journal.date)
+            .font(.system(size: 10))
+            .foregroundColor(textColor)
+        }
+      }
     }
+    .frame(width: 358, height: 80, alignment: .leading)
+    .background(
+      Rectangle()
+        .fill(color)
+        .cornerRadius(16)
+    )
+  }
 }
 
 struct JournalCell_Previews: PreviewProvider {
-    static var emoji = Emotion.data[0]
-    static var previews: some View {
-        JournalCell(journal: Journal(emoji: emoji.emoji, title: "Jogging", note: "Pagi itu saya joging kesana kemari dengan indah", date: "Rab, 27 Juli 2021"))
-            .previewLayout(.fixed(width: 400, height: 100))
-    }
+  static var journal = Journal(emoji: "🫠", title: "tes", note: "tes", date: "2022-07-27T12:14:31Z")
+  static var previews: some View {
+    List(0..<5) { _ in
+      JournalCell(journal: journal)
+        .previewLayout(.fixed(width: 400, height: 100))
+    }.listStyle(.inset)
+  }
 }
