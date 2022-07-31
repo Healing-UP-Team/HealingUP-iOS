37af21ba8d0a27ca4af42c0501ceb52a7a28a917
Muhammad Rifki Widadi <61343742+mrifkiw@users.noreply.github.com>
2022-08-01 00:11:57 +0700

    feat: add view for deep breath session

---
+++ HealingUp-Watch WatchKit Extension/App/Features/DeepBreath/Views/DeepBreathView.swift
@@ -0,0 +1,75 @@
+//
+//  DeepBreathView.swift
+//  Semhas-WatchOS WatchKit Extension
+//
+//  Created by Muhammad Rifki Widadi on 31/07/22.
+//
+
+import SwiftUI
+
+struct DeepBreathView: View {
+  @StateObject var vm = DeepBreathViewModel()
+  @State var progressValue: Float = 0.0
+
+  var body: some View {
+    ZStack {
+      NavigationLink(destination: DeepBreathReport(), isActive: $vm.isSessionCompleted, label: { EmptyView() })
+        .disabled(true)
+        .hidden()
+      TabView {
+        VStack {
+          Spacer()
+          ZStack {
+            ZStack {
+              Circle()
+                .frame(width: 60, height: 60)
+                .foregroundColor(vm.animationColor.opacity(0.4))
+                .scaleEffect(vm.isStartBreathAnimation ? 1.1 : 1)
+                .opacity(vm.isStartBreathAnimation ? 1 : 0)
+              Circle()
+                .frame(width: 66, height: 66)
+                .foregroundColor(vm.animationColor.opacity(0.3))
+                .scaleEffect(vm.isStartBreathAnimation ? 1.3 : 1)
+                .opacity(vm.isStartBreathAnimation ? 1 : 0)
+              Circle()
+                .frame(width: 70, height: 70)
+                .foregroundColor(vm.animationColor.opacity(0.1))
+                .scaleEffect(vm.isStartBreathAnimation ? 1.5 : 1)
+                .opacity(vm.isStartBreathAnimation ? 1 : 0)
+            }
+            .animation(vm.isStartBreathAnimation ?
+              .linear(duration: 4)
+              .repeatForever(autoreverses: true)
+                       : nil,
+                       value: vm.isStartBreathAnimation)
+
+            Text(vm.instruction)
+          }
+          Spacer()
+          Text(vm.counterText)
+        }
+        .onAppear {
+          vm.isStartBreathAnimation = true
+          vm.setupTimer()
+        }
+
+        Button("Stop") {
+          vm.stopTimer()
+          vm.hapticTimer?.cancel()
+          vm.isStartBreathAnimation = false
+        }
+      }
+      .navigationBarHidden(true)
+      .tabViewStyle(.page(indexDisplayMode: .always))
+    .indexViewStyle(.page(backgroundDisplayMode: .automatic))
+    }
+
+
+  }
+}
+
+struct DeepBreathView_Previews: PreviewProvider {
+  static var previews: some View {
+    DeepBreathView()
+  }
+}
