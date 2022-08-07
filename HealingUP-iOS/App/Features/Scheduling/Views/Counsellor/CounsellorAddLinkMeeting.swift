e62632cc8561f3824fd168ed8ac73ad706521805
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-08-07 12:28:39 +0700

    feat(counsellor): add ui for add link

---
+++ HealingUP-iOS/App/Features/Scheduling/Views/Counsellor/CounsellorAddLinkMeeting.swift
@@ -0,0 +1,77 @@
+//
+//  CounsellorAddLinkMeeting.swift
+//  HealingUP-iOS
+//
+//  Created by Muhammad Rifki Widadi on 07/08/22.
+//
+
+import SwiftUI
+import SwiftUICharts
+
+struct CounsellorAddLinkMeeting: View {
+  @State private var link = ""
+  @State private var isLinkFilled = false
+  @Environment(\.presentationMode) private var presentationMode
+  var body: some View {
+    VStack(spacing: 5) {
+      HStack {
+        Text("Tambah Tautan")
+        .font(.system(size: 30))
+        .fontWeight(.bold)
+        .padding(.horizontal)
+        .foregroundColor(.accentPurple)
+        Spacer()
+        Button {
+          presentationMode.wrappedValue.dismiss()
+        } label: {
+          Image(systemName: "xmark")
+            .font(.system(size: 20, weight: .bold))
+            .frame(width: 20, height: 20)
+            .padding()
+            .foregroundColor(Color.accentPurple)
+            .background(
+              Circle()
+                .fill(Color.accentPurple.opacity(0.1))
+            ).padding()
+        }
+
+      }
+
+      VStack(alignment: .leading) {
+          TextEditor(text: $link)
+            .padding()
+            .background(
+              RoundedRectangle(cornerRadius: 16)
+                .stroke(Color.accentPurple, lineWidth: 4)
+            )
+            .padding()
+          HStack {
+            Spacer()
+            Button("Tambahkan Tautan") {
+              if link.isEmpty {
+                isLinkFilled.toggle()
+              } else {
+                presentationMode.wrappedValue.dismiss()
+              }
+            }.foregroundColor(.white)
+            Spacer()
+          }
+          .padding()
+          .background(Color.accentPurple)
+          .cornerRadius(12)
+          .padding(.horizontal)
+          Spacer()
+
+        .alert(isPresented: $isLinkFilled) {
+          Alert(title: Text("Link belum diisi"), message: Text("Apakah kamu yakin sudah mengisi link?"), dismissButton: .default(Text("Ok, Saya mengerti")))
+        }
+      }
+    }
+  }
+}
+
+struct CounsellorAddLinkMeeting_Previews: PreviewProvider {
+  static var previews: some View {
+    CounsellorAddLinkMeeting()
+  }
+}
